f3cacc911a8ec7207b95095a5ba5b3e768f62ab8
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-07-15 16:35:19 -0700

    testing first try

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,83 @@
-// IMPORT MODULES under test here:
-// import { example } from '../example.js';
+// // IMPORT MODULES under test here:
+// // import { example } from '../example.js';
 
+// const test = QUnit.test;
+
+// test('time to test a function', (expect) => {
+//     //Arrange
+//     // Set up your arguments and expectations
+//     const expected = true;
+    
+//     //Act 
+//     // Call the function you're testing and set the result to a const
+//     const actual = true;
+
+//     //Expect
+//     // Make assertions about what is expected versus the actual result
+//     expect.equal(actual, expected);
+// });
+
+// IMPORT MODULES under test here:
+import { add } from '../utils.js';
+import { subtract } from '../utils.js';
+import { multiply } from '../utils.js';
+import { divide } from '../utils.js';
+
+// using a test library
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('add should take in 5 and 3 and return 8', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 8;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = add(5, 3);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('subtract should take inputs 7 and 5 and return 2', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 2;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = subtract(7, 5);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('multiply should take 2 and 4 and output 8', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 8;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = multiply(2, 4);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('divide should take 10 and 2 and output 5', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 5;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = divide(10, 2);
 
     //Expect
     // Make assertions about what is expected versus the actual result
---
+++ utils.js
@@ -0,0 +1,15 @@
+export function add(booger1, booger2) {
+    return Number(booger1) + Number(booger2);
+}
+
+export function subtract(booger1, booger2) {
+    return Number(booger1) - Number(booger2);
+}
+
+export function multiply(booger1, booger2) {
+    return Number(booger1) * Number(booger2);
+}
+
+export function divide(booger1, booger2) {
+    return Number(booger1) / Number(booger2);
+}
